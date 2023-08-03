1d7c700ecd72bb67918e107baa8d7106b993331a
Phu Duc Nguyen <sillycat@duck.com>
2023-08-03 23:24:14 +0700

    Add simple Log in/out form

---
+++ script.js
@@ -0,0 +1,38 @@
+document.getElementById('loginForm').addEventListener('submit', (event) => {
+  event.preventDefault();
+  const form = event.target;
+  const username = form.username.value;
+  const password = form.password.value;
+  // Add your authentication logic here
+  if (username === 'user' && password === 'pass') {
+    showLogout();
+    clearErrorMessage();
+  } else {
+    showErrorMessage('Invalid username or password.');
+  }
+  form.reset();
+});
+
+document.getElementById('logoutBtn').addEventListener('click', () => {
+  showLogin();
+});
+
+function showErrorMessage(message) {
+  const errorMessage = document.getElementById('errorMessage');
+  errorMessage.textContent = message;
+}
+
+function clearErrorMessage() {
+  const errorMessage = document.getElementById('errorMessage');
+  errorMessage.textContent = '';
+}
+
+function showLogin() {
+  document.getElementById('loginForm').style.display = 'block';
+  document.getElementById('logoutBtn').style.display = 'none';
+}
+
+function showLogout() {
+  document.getElementById('loginForm').style.display = 'none';
+  document.getElementById('logoutBtn').style.display = 'block';
+}
