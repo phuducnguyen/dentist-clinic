4c6b9275898b957025c6671cea7456f74cf29b9f
Phu Duc Nguyen <sillycat@duck.com>
2023-08-04 00:58:14 +0700

    Create React-App

---
+++ dentist-clinic-management/src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h1>Welcome to Dentist Clinic Management System</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent
+        libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.
+        Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa.
+        Vestibulum lacinia arcu eget nulla.
+      </p>
+      <p>
+        Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec
+        tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
